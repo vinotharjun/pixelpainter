d729eabed3fdaf41a60c5b627cb8838851782b88
vinotharjun <vinod.looser00@gmail.com>
2020-07-21 22:27:10 +0530

    minor fix on daliiterloader

--- painter/imports/torchpacks.py
+++ painter/imports/torchpacks.py
@@ -4,7 +4,7 @@ from torch import nn, optim, as_tensor
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import models
+from torchvision import models, transforms
 import torchvision
 from torch.utils.data import BatchSampler, DataLoader, Dataset, Sampler, TensorDataset
 from torch.nn.utils import weight_norm, spectral_norm
