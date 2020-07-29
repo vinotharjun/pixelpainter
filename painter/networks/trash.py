1f1b92a3c0e205d2d5d679ee37dd5fc9a15585bc
vinotharjun <vinotharjun@hotmail.com>
2020-07-29 23:40:35 +0530

    update trash

--- painter/networks/trash.py
+++ painter/networks/trash.py
@@ -1,197 +1,187 @@
-  
-import math
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import models
 
 
-def weights_init(init_type='gaussian'):
-    def init_fun(m):
-        classname = m.__class__.__name__
-        if (classname.find('Conv') == 0 or classname.find(
-                'Linear') == 0) and hasattr(m, 'weight'):
-            if init_type == 'gaussian':
-                nn.init.normal_(m.weight, 0.0, 0.02)
-            elif init_type == 'xavier':
-                nn.init.xavier_normal_(m.weight, gain=math.sqrt(2))
-            elif init_type == 'kaiming':
-                nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
-            elif init_type == 'orthogonal':
-                nn.init.orthogonal_(m.weight, gain=math.sqrt(2))
-            elif init_type == 'default':
-                pass
-            else:
-                assert 0, "Unsupported initialization: {}".format(init_type)
-            if hasattr(m, 'bias') and m.bias is not None:
-                nn.init.constant_(m.bias, 0.0)
+class PartialConvLayer (nn.Module):
 
-    return init_fun
+	def __init__(self, in_channels, out_channels, bn=True, bias=False, sample="none-3", activation="relu"):
+		super().__init__()
+		self.bn = bn
 
+		if sample == "down-7":
+			# Kernel Size = 7, Stride = 2, Padding = 3
+			self.input_conv = nn.Conv2d(in_channels, out_channels, 7, 2, 3, bias=bias)
+			self.mask_conv = nn.Conv2d(in_channels, out_channels, 7, 2, 3, bias=False)
 
-class VGG16FeatureExtractor(nn.Module):
-    def __init__(self):
-        super().__init__()
-        vgg16 = models.vgg16(pretrained=True)
-        self.enc_1 = nn.Sequential(*vgg16.features[:5])
-        self.enc_2 = nn.Sequential(*vgg16.features[5:10])
-        self.enc_3 = nn.Sequential(*vgg16.features[10:17])
+		elif sample == "down-5":
+			self.input_conv = nn.Conv2d(in_channels, out_channels, 5, 2, 2, bias=bias)
+			self.mask_conv = nn.Conv2d(in_channels, out_channels, 5, 2, 2, bias=False)
 
-        # fix the encoder
-        for i in range(3):
-            for param in getattr(self, 'enc_{:d}'.format(i + 1)).parameters():
-                param.requires_grad = False
+		elif sample == "down-3":
+			self.input_conv = nn.Conv2d(in_channels, out_channels, 3, 2, 1, bias=bias)
+			self.mask_conv = nn.Conv2d(in_channels, out_channels, 3, 2, 1, bias=False)
 
-    def forward(self, image):
-        results = [image]
-        for i in range(3):
-            func = getattr(self, 'enc_{:d}'.format(i + 1))
-            results.append(func(results[-1]))
-        return results[1:]
+		else:
+			self.input_conv = nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=bias)
+			self.mask_conv = nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False)
 
+		nn.init.constant_(self.mask_conv.weight, 1.0)
 
-class PartialConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True):
-        super().__init__()
-        self.input_conv = nn.Conv2d(in_channels, out_channels, kernel_size,
-                                    stride, padding, dilation, groups, bias)
-        self.mask_conv = nn.Conv2d(in_channels, out_channels, kernel_size,
-                                   stride, padding, dilation, groups, False)
-        self.input_conv.apply(weights_init('kaiming'))
+		# "Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification"
+		# negative slope of leaky_relu set to 0, same as relu
+		# "fan_in" preserved variance from forward pass
+		nn.init.kaiming_normal_(self.input_conv.weight, a=0, mode="fan_in")
 
-        torch.nn.init.constant_(self.mask_conv.weight, 1.0)
+		for param in self.mask_conv.parameters():
+			param.requires_grad = False
 
-        # mask is not updated
-        for param in self.mask_conv.parameters():
-            param.requires_grad = False
+		if bn:
+			# Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift
+			# Applying BatchNorm2d layer after Conv will remove the channel mean
+			self.batch_normalization = nn.BatchNorm2d(out_channels)
 
-    def forward(self, input, mask):
-        # http://masc.cs.gmu.edu/wiki/partialconv
-        # C(X) = W^T * X + b, C(0) = b, D(M) = 1 * M + 0 = sum(M)
-        # W^T* (M .* X) / sum(M) + b = [C(M .* X) – C(0)] / D(M) + C(0)
+		if activation == "relu":
+			# Used between all encoding layers
+			self.activation = nn.ReLU()
+		elif activation == "leaky_relu":
+			# Used between all decoding layers (Leaky RELU with alpha = 0.2)
+			self.activation = nn.LeakyReLU(negative_slope=0.2)
 
-        output = self.input_conv(input * mask)
-        if self.input_conv.bias is not None:
-            output_bias = self.input_conv.bias.view(1, -1, 1, 1).expand_as(
-                output)
-        else:
-            output_bias = torch.zeros_like(output)
+	def forward(self, input_x, mask):
+		# output = W^T dot (X .* M) + b
+		output = self.input_conv(input_x * mask)
 
-        with torch.no_grad():
-            output_mask = self.mask_conv(mask)
+		# requires_grad = False
+		with torch.no_grad():
+			# mask = (1 dot M) + 0 = M
+			output_mask = self.mask_conv(mask)
 
-        no_update_holes = output_mask == 0
-        mask_sum = output_mask.masked_fill_(no_update_holes, 1.0)
+		if self.input_conv.bias is not None:
+			# spreads existing bias values out along 2nd dimension (channels) and then expands to output size
+			output_bias = self.input_conv.bias.view(1, -1, 1, 1).expand_as(output)
+		else:
+			output_bias = torch.zeros_like(output)
 
-        output_pre = (output - output_bias) / mask_sum + output_bias
-        output = output_pre.masked_fill_(no_update_holes, 0.0)
+		# mask_sum is the sum of the binary mask at every partial convolution location
+		mask_is_zero = (output_mask == 0)
+		# temporarily sets zero values to one to ease output calculation 
+		mask_sum = output_mask.masked_fill_(mask_is_zero, 1.0)
 
-        new_mask = torch.ones_like(output)
-        new_mask = new_mask.masked_fill_(no_update_holes, 0.0)
+		# output at each location as follows:
+		# output = (W^T dot (X .* M) + b - b) / M_sum + b ; if M_sum > 0
+		# output = 0 ; if M_sum == 0
+		output = (output - output_bias) / mask_sum + output_bias
+		output = output.masked_fill_(mask_is_zero, 0.0)
 
-        return output, new_mask
+		# mask is updated at each location
+		new_mask = torch.ones_like(output)
+		new_mask = new_mask.masked_fill_(mask_is_zero, 0.0)
+
+		if self.bn:
+			output = self.batch_normalization(output)
+
+		if hasattr(self, 'activation'):
+			output = self.activation(output)
+
+		return output, new_mask
 
 
-class PCBActiv(nn.Module):
-    def __init__(self, in_ch, out_ch, bn=True, sample='none-3', activ='relu',
-                 conv_bias=False):
-        super().__init__()
-        if sample == 'down-5':
-            self.conv = PartialConv(in_ch, out_ch, 5, 2, 2, bias=conv_bias)
-        elif sample == 'down-7':
-            self.conv = PartialConv(in_ch, out_ch, 7, 2, 3, bias=conv_bias)
-        elif sample == 'down-3':
-            self.conv = PartialConv(in_ch, out_ch, 3, 2, 1, bias=conv_bias)
-        else:
-            self.conv = PartialConv(in_ch, out_ch, 3, 1, 1, bias=conv_bias)
+class PartialConvUNet(nn.Module):
 
-        if bn:
-            self.bn = nn.BatchNorm2d(out_ch)
-        if activ == 'relu':
-            self.activation = nn.ReLU()
-        elif activ == 'leaky':
-            self.activation = nn.LeakyReLU(negative_slope=0.2)
+	# 256 x 256 image input, 256 = 2^8
+	def __init__(self, input_size=256, layers=7):
+		if 2 ** (layers + 1) != input_size:
+			raise AssertionError
 
-    def forward(self, input, input_mask):
-        h, h_mask = self.conv(input, input_mask)
-        if hasattr(self, 'bn'):
-            h = self.bn(h)
-        if hasattr(self, 'activation'):
-            h = self.activation(h)
-        return h, h_mask
+		super().__init__()
+		self.freeze_enc_bn = False
+		self.layers = layers
 
+		# ======================= ENCODING LAYERS =======================
+		# 3x256x256 --> 64x128x128
+		self.encoder_1 = PartialConvLayer(3, 64, bn=False, sample="down-7")
 
-class PConvUNet(nn.Module):
-    def __init__(self, layer_size=7, input_channels=3, upsampling_mode='nearest'):
-        super().__init__()
-        self.freeze_enc_bn = False
-        self.upsampling_mode = upsampling_mode
-        self.layer_size = layer_size
-        self.enc_1 = PCBActiv(input_channels, 64, bn=False, sample='down-7')
-        self.enc_2 = PCBActiv(64, 128, sample='down-5')
-        self.enc_3 = PCBActiv(128, 256, sample='down-5')
-        self.enc_4 = PCBActiv(256, 512, sample='down-3')
-        for i in range(4, self.layer_size):
-            name = 'enc_{:d}'.format(i + 1)
-            setattr(self, name, PCBActiv(512, 512, sample='down-3'))
+		# 64x128x128 --> 128x64x64
+		self.encoder_2 = PartialConvLayer(64, 128, sample="down-5")
 
-        for i in range(4, self.layer_size):
-            name = 'dec_{:d}'.format(i + 1)
-            setattr(self, name, PCBActiv(512 + 512, 512, activ='leaky'))
-        self.dec_4 = PCBActiv(512 + 256, 256, activ='leaky')
-        self.dec_3 = PCBActiv(256 + 128, 128, activ='leaky')
-        self.dec_2 = PCBActiv(128 + 64, 64, activ='leaky')
-        self.dec_1 = PCBActiv(64 + input_channels, input_channels,
-                              bn=False, activ=None, conv_bias=True)
+		# 128x64x64 --> 256x32x32
+		self.encoder_3 = PartialConvLayer(128, 256, sample="down-3")
 
-    def forward(self, input, input_mask):
-        h_dict = {}  # for the output of enc_N
-        h_mask_dict = {}  # for the output of enc_N
+		# 256x32x32 --> 512x16x16
+		self.encoder_4 = PartialConvLayer(256, 512, sample="down-3")
 
-        h_dict['h_0'], h_mask_dict['h_0'] = input, input_mask
+		# 512x16x16 --> 512x8x8 --> 512x4x4 --> 512x2x2
+		for i in range(5, layers + 1):
+			name = "encoder_{:d}".format(i)
+			setattr(self, name, PartialConvLayer(512, 512, sample="down-3"))
 
-        h_key_prev = 'h_0'
-        for i in range(1, self.layer_size + 1):
-            l_key = 'enc_{:d}'.format(i)
-            h_key = 'h_{:d}'.format(i)
-            h_dict[h_key], h_mask_dict[h_key] = getattr(self, l_key)(
-                h_dict[h_key_prev], h_mask_dict[h_key_prev])
-            h_key_prev = h_key
+		# ======================= DECODING LAYERS =======================
+		# dec_7: UP(512x2x2) + 512x4x4(enc_6 output) = 1024x4x4 --> 512x4x4
+		# dec_6: UP(512x4x4) + 512x8x8(enc_5 output) = 1024x8x8 --> 512x8x8
+		# dec_5: UP(512x8x8) + 512x16x16(enc_4 output) = 1024x16x16 --> 512x16x16
+		for i in range(5, layers + 1):
+			name = "decoder_{:d}".format(i)
+			setattr(self, name, PartialConvLayer(512 + 512, 512, activation="leaky_relu"))
 
-        h_key = 'h_{:d}'.format(self.layer_size)
-        h, h_mask = h_dict[h_key], h_mask_dict[h_key]
+		# UP(512x16x16) + 256x32x32(enc_3 output) = 768x32x32 --> 256x32x32
+		self.decoder_4 = PartialConvLayer(512 + 256, 256, activation="leaky_relu")
 
-        # concat upsampled output of h_enc_N-1 and dec_N+1, then do dec_N
-        # (exception)
-        #                            input         dec_2            dec_1
-        #                            h_enc_7       h_enc_8          dec_8
+		# UP(256x32x32) + 128x64x64(enc_2 output) = 384x64x64 --> 128x64x64
+		self.decoder_3 = PartialConvLayer(256 + 128, 128, activation="leaky_relu")
 
-        for i in range(self.layer_size, 0, -1):
-            enc_h_key = 'h_{:d}'.format(i - 1)
-            dec_l_key = 'dec_{:d}'.format(i)
+		# UP(128x64x64) + 64x128x128(enc_1 output) = 192x128x128 --> 64x128x128
+		self.decoder_2 = PartialConvLayer(128 + 64, 64, activation="leaky_relu")
 
-            h = F.interpolate(h, scale_factor=2, mode=self.upsampling_mode)
-            h_mask = F.interpolate(
-                h_mask, scale_factor=2, mode='nearest')
+		# UP(64x128x128) + 3x256x256(original image) = 67x256x256 --> 3x256x256(final output)
+		self.decoder_1 = PartialConvLayer(64 + 3, 3, bn=False, activation="", bias=True)
+	
+	def forward(self, input_x, mask):
+		encoder_dict = {}
+		mask_dict = {}
 
-            h = torch.cat([h, h_dict[enc_h_key]], dim=1)
-            h_mask = torch.cat([h_mask, h_mask_dict[enc_h_key]], dim=1)
-            h, h_mask = getattr(self, dec_l_key)(h, h_mask)
-        h+=input
+		key_prev = "h_0"
+		encoder_dict[key_prev], mask_dict[key_prev] = input_x, mask
 
-        return h, h_mask
+		for i in range(1, self.layers + 1):
+			encoder_key = "encoder_{:d}".format(i)
+			key = "h_{:d}".format(i)
+			# Passes input and mask through encoding layer
+			encoder_dict[key], mask_dict[key] = getattr(self, encoder_key)(encoder_dict[key_prev], mask_dict[key_prev])
+			key_prev = key
 
-    def train(self, mode=True):
-        """
-        Override the default train() to freeze the BN parameters
-        """
-        super().train(mode)
-        if self.freeze_enc_bn:
-            for name, module in self.named_modules():
-                if isinstance(module, nn.BatchNorm2d) and 'enc' in name:
-                    module.eval()
+		# Gets the final output data and mask from the encoding layers
+		# 512 x 2 x 2
+		out_key = "h_{:d}".format(self.layers)
+		out_data, out_mask = encoder_dict[out_key], mask_dict[out_key]
+
+		for i in range(self.layers, 0, -1):
+			encoder_key = "h_{:d}".format(i - 1)
+			decoder_key = "decoder_{:d}".format(i)
+
+			# Upsample to 2 times scale, matching dimensions of previous encoding layer output
+			out_data = F.interpolate(out_data, scale_factor=2)
+			out_mask = F.interpolate(out_mask, scale_factor=2)
+
+			# concatenate upsampled decoder output with encoder output of same H x W dimensions
+			# s.t. final decoding layer input will contain the original image
+			out_data = torch.cat([out_data, encoder_dict[encoder_key]], dim=1)
+			# also concatenate the masks
+			out_mask = torch.cat([out_mask, mask_dict[encoder_key]], dim=1)
+			
+			# feed through decoder layers
+			out_data, out_mask = getattr(self, decoder_key)(out_data, out_mask)
+
+		return out_data
+
+	def train(self, mode=True):
+		super().train(mode)
+		if self.freeze_enc_bn:
+			for name, module in self.named_modules():
+				if isinstance(module, nn.BatchNorm2d) and "enc" in name:
+					# Sets batch normalization layers to evaluation mode
+					module.eval()
+
 
 import torch
 import torch.nn as nn
